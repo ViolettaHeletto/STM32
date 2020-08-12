14255d78ac6f2989378e67d34bbebb475033450f
ViolettaHeletto <heletto.violetta@gmail.com>
2020-08-12 12:48:41 +0300

    New header
    upgrated_heartbeat.h

---
+++ Src/upgrated_heartbeat.h
@@ -0,0 +1,13 @@
+/*
+ * upgrated_heartbeat.h
+ *
+ *  Created on: 12 авг. 2020 г.
+ *      Author: Violetta
+ */
+
+#ifndef UPGRATED_HEARTBEAT_H_
+#define UPGRATED_HEARTBEAT_H_
+
+
+
+#endif /* UPGRATED_HEARTBEAT_H_ */
