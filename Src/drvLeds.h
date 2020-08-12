8b4cd616f9cdbbf8475a3990442e448b6240f0ae
ViolettaHeletto <heletto.violetta@gmail.com>
2020-08-12 16:53:44 +0300

    It's upgrated heartbeat
    With new header file

--- Src/drvLeds.h
+++ Src/drvLeds.h
@@ -7,7 +7,14 @@
 
 #ifndef DRVLEDS_H_
 #define DRVLEDS_H_
+#include "main.h"
 
+typedef enum {
+	LED_GREEN =  LD4_Pin,
+	LED_RED =  LD5_Pin,
+	LED_Blue = LD6_Pin,
+	LED_Orange =  LD3_Pin
+}leds_t;
 
 
 #endif /* DRVLEDS_H_ */
