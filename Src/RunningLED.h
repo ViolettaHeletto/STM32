0a568326ee0824f79979e0ec6aa9fb014e47deb4
ViolettaHeletto <heletto.violetta@gmail.com>
2020-08-23 21:17:30 +0300

    It is file with LED`s running
    It is for Multi threading with heartbeat

---
+++ Src/RunningLED.h
@@ -0,0 +1,19 @@
+/*
+ * RunningLED.h
+ *
+ *  Created on: 23 авг. 2020 г.
+ *      Author: Violetta
+ */
+
+#ifndef RUNNINGLED_H_
+#define RUNNINGLED_H_
+
+
+#include "upgrated_heartbeat.h"
+#include "ulSysTime.h"
+
+//void RunningLEDs(uint16_t DutyCycle, uint16_t period);
+void RunningLEDs(uint16_t PERIOD, uint16_t DUTY_CYCLE);
+
+
+#endif /* RUNNINGLED_H_ */
